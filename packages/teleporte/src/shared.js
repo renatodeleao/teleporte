216f099715db0fb962f93e535734eb982aa3d7ef
Renato de Leão <renatodeleao@gmail.com>
2024-07-19 23:14:40 +0100

    refactor: some lint suggestions

--- packages/teleporte/src/shared.js
+++ packages/teleporte/src/shared.js
@@ -2,15 +2,17 @@ import { provide, getCurrentInstance } from 'vue'
 
 /**
  * Extracts the provided context from the current instance allowing to forward
- * it into the teleported component.
- * @returns
+ * it into the teleported component by calling the returned function.
+ * @returns {() => void}
  */
 export function useProvidedContext() {
   const ctx = getCurrentInstance()
 
   return function forwardContext() {
-    Object.entries(ctx.provides).forEach(([key, context]) => {
+    // @ts-ignore
+    for ([key, context] of Object.entries(ctx.provides)) {
+      // @ts-ignore
       provide(key, context)
-    })
+    }
   }
 }
