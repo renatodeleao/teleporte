e0db34d8c18ecd342f62974cf1c74876ab6c1ac6
Renato de Leão <renatodeleao@gmail.com>
2024-07-30 17:10:23 +0100

    feat(playground-ssr): add a sample nuxt app to test ssr

---
+++ apps/playground-ssr/app.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { TeleportOrigin, TeleportTarget } from 'teleporte'
+const disabled = ref(false)
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <button @click="disabled = !disabled">Toggle disabled</button>
+    <div class="showcase">
+      <div class="origin">
+        <TeleportOrigin
+          to="target"
+          :disabled="disabled"
+        >
+          <div class="teleport">
+            Teleported content
+          </div>
+        </TeleportOrigin>
+      </div>
+
+      <div class="target">
+        <TeleportTarget
+          name="target"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.origin, .target {
+  height: 100px;
+}
+.origin { border: 1px solid red; }
+.target { border: 1px solid blue; }
+</style>
